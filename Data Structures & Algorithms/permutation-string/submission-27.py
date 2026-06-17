ab413761e6a965ea29f812e76873b869f3d6c878
Rajan Grewal <rajangrewal123@gmail.com>
2026-06-17 14:11:23 -0700

    Add: permutation-string - submission-27

---
+++ Data Structures & Algorithms/permutation-string/submission-27.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        freq = [0] * 26
+        l, r = 0, 0
+        
+        def pos(char):
+            return ord(char) - ord('a')
+
+        for c in s1:
+            freq[pos(c)] += 1
+
+        while r < len(s2) and r - l < len(s1):
+            if freq[pos(s2[r])]:
+                freq[pos(s2[r])] -= 1
+                r += 1
+                print(s2[l:r], r)
+            elif l == r:
+                l += 1
+                r += 1
+            else:
+                freq[pos(s2[l])] += 1
+                l += 1
+        return r - l == len(s1)
