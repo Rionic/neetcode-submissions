dc0ef4bb72124a5f0d7647e7e21625574b9cddfc
Rajan Grewal <rajangrewal123@gmail.com>
2026-07-04 16:15:09 -0700

    Add: remove-all-adjacent-duplicates-in-string-ii - submission-3

---
+++ Data Structures & Algorithms/remove-all-adjacent-duplicates-in-string-ii/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        st = []
+        for c in s:
+            if st and st[-1][0] == c: # repeat letter
+                if st[-1][1] == k - 1: # k-length str -> remove
+                    st.pop()
+                else:
+                    st[-1][1] += 1 # increment repeat char count
+            else:
+                st.append([c, 1]) # non-repeat letter
+        res = [c*f for c, f in st]
+
+        return ''.join(res)
