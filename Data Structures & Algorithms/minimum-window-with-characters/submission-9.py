fa311d7eabcf58652fd3cc323a6b8e511216632d
Rajan Grewal <rajangrewal123@gmail.com>
2026-07-24 10:32:56 -0700

    Add: minimum-window-with-characters - submission-9

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-9.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        l, res = 0, ''
+        charMap = defaultdict(int)
+        for c in t:
+            charMap[c] += 1
+        for r in range(len(s)):
+            if s[r] in t:
+                charMap[s[r]] -= 1
+
+            while max(charMap.values()) == 0: # Found substring
+                if res == '' or r - l + 1 < len(res):
+                    res = s[l:r + 1]
+                if s[l] in charMap:
+                    charMap[s[l]] += 1
+                l += 1
+
+        return res
+            
