309c82f50ecfe8931cdaed1e8ed2800000c859df
Rajan Grewal <rajangrewal123@gmail.com>
2026-08-02 11:28:02 -0700

    Add: jump-game-ii - submission-3

---
+++ Data Structures & Algorithms/jump-game-ii/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        if len(nums) <= 1: return 0
+        l, r = 1, nums[0]
+        minJumps = 1
+        while l <= r and r < len(nums) - 1:
+            bestJump = 0
+            while l <= r:
+                bestJump = max(nums[l] + l, bestJump)
+                l += 1
+            r = bestJump
+            minJumps += 1
+
+        return minJumps
