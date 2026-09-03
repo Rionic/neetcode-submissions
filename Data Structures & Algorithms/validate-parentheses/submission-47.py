eac94875a80501d8908057ff2276163caa3edf3b
Rajan Grewal <rajangrewal123@gmail.com>
2026-09-03 16:51:27 -0700

    Add: validate-parentheses - submission-47

---
+++ Data Structures & Algorithms/validate-parentheses/submission-47.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        bm = {')': '(', '}': '{', ']': '['}
+        st = []
+        for b in s:
+            if b in bm.keys(): #closing
+                if not st or st[-1] != bm[b]:
+                    return False
+                st.pop()
+            else:
+                st.append(b)
+        
+        return len(st) == 0
